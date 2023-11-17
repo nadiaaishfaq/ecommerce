dae5c30aedca74a97eaa5cbbced76c0998e39710
nadiaaishfaq <nadiaaishfaq@gmail.com>
2023-11-17 20:24:04 +0500

    completed

--- server/routers/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const authControllers = require('../controllers/authControllers')
-
-router.post('/sign-up', authControllers.signUp)
-router.post('/login', authControllers.logIn)
-
-module.exports = router
--- server/routers/productRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const createProduct = require('../controllers/productControllers')
-const upload = require('../middlewares/upload')
-
-router.post('/createProduct', upload.single('img'), createProduct)
-
-module.exports = router
